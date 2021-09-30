198a9172b6bffd4e0f8265a9897852eb04ec70fb
Albaro Garzon Funes <sc17657@fiatauto.adfa.local>
2021-09-30 09:54:28 -0300

    Adding features

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
-const morgan = require("morgan");
-const exphbs = require("express-handlebars");
-const path = require("path");
-const passport = require("passport");
-const session = require("express-session");
-const MongoDbStore = require("connect-mongo");
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override')
+const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
+const MongoDbStore = require('connect-mongo');
 
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 connectDB();
 
 const app = express();
 
-if (process.env.NODE_ENV == "development") app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
-app.set("view engine", ".hbs");
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
+
+if (process.env.NODE_ENV == 'development') app.use(morgan('dev'));
+
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs');
+
+app.engine(
+  '.hbs',
+  exphbs({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select
+    },
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+);
+app.set('view engine', '.hbs');
 
 app.use(
   session({
-    secret: "keyboard cat",
+    secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
     store: MongoDbStore.create({ mongoUrl: process.env.MONGO_URI }),
@@ -34,10 +64,16 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
 
-app.use("/", require("./routes/index"));
-app.use("/auth", require("./routes/auth"));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 const PORT = process.env.PORT || 3000;
 
